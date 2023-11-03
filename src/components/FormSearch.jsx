a292bef88d88b6c73f79fd335f629a31e11fb70e
mago_d_Mercurio <davila.feria@gmail.com>
2023-11-02 22:23:58 -0500

    form search trabajando input

--- src/components/FormSearch.jsx
+++ src/components/FormSearch.jsx
@@ -1,6 +1,26 @@
+import { useState } from 'react'
 
+const API = process.env.API_URL
+const VERSION = process.env.API_URL_VERSION
+const API_KEY = process.env.API_KEY
+const URL = `${API}/${VERSION}/`
+
+const searchCity = async (city) => {
+  const response = await fetch(`${URL}search.json?q=${city}&key=${API_KEY}`)
+  const data = await response.json()
+  return data
+  }
 
 export default function FormSearch() {
+
+  const [sCity, setSCity] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const data = await searchCity(sCity)
+    console.log(data)
+  }
+
   return (
     <>
       <form 
@@ -14,14 +34,28 @@ export default function FormSearch() {
         >
           ciudad
         </label>
-        <input type="text"
-          name="current"
-          id="current"
+        <input className="border-2 border-gray-300 hover:border-blue-400 rounded-xl p-2 text-sm text-black"
+          type="text"
+          name="city"
+          id="city"
           placeholder="Escribe tu ciudad"
+          onChange= {(e) => setSCity(e.target.value)}
+          value={sCity}
+          required
+          autoComplete="on"
+          maxLength={50}
+          minLength={3}
+          spellCheck="false"
+
           autoFocus
-          className="border-2 border-gray-300 hover:border-blue-400 rounded-xl p-2 text-sm text-black"
         />
+        <button className="bg-blue-400 text-white rounded-xl p-2 text-sm"
+        onClick={handleSubmit}
+        >
+          Buscar
+        </button>
       </form>
     </>
   )
+  
 }
--- src/services/api/index.js
+++ src/services/api/index.js
@@ -3,5 +3,8 @@ const VERSION = process.env.API_URL_VERSION
 const API_KEY = process.env.API_KEY
 const URL = `${API}/${VERSION}/`
 
-
-console.log(URl)
+const endPoints = {
+  currentWeather: {
+    getCurrentWeather: `${URL}weather?q=London,uk&appid=${API_KEY}`
+  }
+}
