c49e7f87e29cddb75f41e2922fa20df42aab2403
mago_d_Mercurio <davila.feria@gmail.com>
2023-11-22 14:55:18 -0600

    Update CurrentCard.jsx

--- src/components/CurrentCard.jsx
+++ src/components/CurrentCard.jsx
@@ -1,10 +1,21 @@
-
+import useSearchCity from "@/hooks/useSearchCity"
 
 export default function CurrentCard() {
+
+  const { weatherData, loading, error } = useSearchCity()
   return (
     <>
     <aside className="flex justify-center mt-8">
-      Card
+      <h2>Clima de Hoy</h2>
+    {loading && <p>Cargando...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {weatherData && (
+        <div>
+          {/* Renderiza los datos del clima según tus necesidades */}
+          <p>Temperatura: {weatherData.current.temp_c}°C</p>
+          {/* Agrega más elementos según sea necesario */}
+        </div>
+      )}
     </aside>
     </>
   )
