dfbfd23296960b3fd7396c40ba22ea357d86d3a8
mago_d_Mercurio <davila.feria@gmail.com>
2023-11-29 15:59:20 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import Link from "next/link"
-import { Icon } from '@iconify/react';
 
 const socialLinks = [
   { label: "LinkedIn", href: "https://www.linkedin.com/in/rauldavilaf/", icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"><path d="M4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm4 5v5m0-8v.01M12 16v-5"/><path d="M16 16v-3a2 2 0 0 0-4 0"/></g></svg> },
